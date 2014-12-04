6ba12af4ddb87d61749c82c66222aad103ada3be
hasan rafiq <rafiqhasan@gmail.com>
2014-12-04 17:36:44 +0530

    Sam Second

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  attr_accessor :password , :password_confirmation
+  attr_accessible :email, :enc_passwd, :f_name, :l_name, :vendor_flag, :password, :password_confirmation
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :f_name,  :presence => true,
+                    :length => { :maximum => 50 }
+
+  validates :email, :presence => true,
+                    :format => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password,  :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40 }
 end
